63a8f059af3cc6a9b66ed295dc52e65185b50015
geetanjalising <geetanjalisingh1815@gmail.com>
2023-09-17 07:27:45 +0530

    error correction

--- server.js
+++ server.js
@@ -24,14 +24,6 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
  
-//Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  //Set static folder 
-  app.use(express.static('client/build'));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
